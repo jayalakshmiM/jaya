d7a9cbc17259115a6c0eee5746f5ef3bfb119529
jayalakshmiM <jayal3393@gmail.com>
2017-06-20 21:40:25 +0530

    Create pnz.java

---
+++ pnz.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pnz 
+{
+	public static void main(String[] args)
+	{
+		  Scanner in=new Scanner(System.in);
+	      int n=in.nextInt();
+	      {
+	      if(n>0)
+	          System.out.println("pos");
+	          else if (n<0)
+	                System.out.println("neg");
+	          else
+	        	  System.out.println("zero");
+	      }
+	  }
+	}
