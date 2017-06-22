3cfd3287c32f49933d2bb6070ec8dec48d8d1390
jayalakshmiM <jayal3393@gmail.com>
2017-06-22 20:43:15 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class natural 
+{
+	public static void main(String[] args)
+  {
+		 int x, i = 1 ;
+	        int sum = 0;
+	        Scanner s = new Scanner(System.in);
+	        x = s.nextInt();
+	        while(i <= x)
+	        {
+	            sum = sum +i;
+	            i++;
+	        }
+	        System.out.println("result="+sum);
+	}
+
+}
