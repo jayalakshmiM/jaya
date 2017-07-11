4a6bc4ef98bd5811e7069131c79f9e6c8f46712c
jayalakshmiM <jayal3393@gmail.com>
2017-07-11 21:50:48 +0530

    Create star.java

---
+++ star.java
@@ -0,0 +1,17 @@
+public class star 
+{
+	public static void main(String[] args)
+	{
+		 int i,j,n=5;
+	        for(i=1;i<=n;i++)
+	        {
+	            for(j=1;j<=n;j++)
+	            {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+
+	}
+
+}
