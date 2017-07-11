b145a781944bd296c479f0592b9ef1d318ce83cc
jayalakshmiM <jayal3393@gmail.com>
2017-07-11 21:45:29 +0530

    Create oddevepos.java

---
+++ oddevepos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class oddevepos 
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		String[] str=s.nextLine().split(" ");
+		for(int i=0;i<str.length;i++)
+		{
+		    if(i==0||i%2==0)
+		    {
+		        StringBuffer sb=new StringBuffer(str[i]);
+		        System.out.print(sb.reverse()+" ");
+		    }
+		    else
+		    {
+		        System.out.print(str[i]+" ");
+		    }
+		}
+              
+	}
+
+}
