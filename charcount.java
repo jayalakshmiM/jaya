898c0a83ee1b87c46f78e6286923d715843d46d3
jayalakshmiM <jayal3393@gmail.com>
2017-07-13 11:45:59 +0530

    Create charcount.java

---
+++ charcount.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class charcount
+{
+	public static void main(String[] args)
+  {
+         Scanner in=new Scanner(System.in);
+         String s=in.next();
+         int s1=s.length();
+         System.out.println(s1);
+      
+	}
+
+}
