d303270ebc02fc7215d8e0522c4670782ac87aab
jayalakshmiM <jayal3393@gmail.com>
2017-07-17 15:08:39 +0530

    Create coin.cpp

---
+++ coin.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+	#define read cin>>
+	#define out  cout<<
+	using namespace std;
+	
+	long int num=0;
+	int number_of_coin(long int sum)
+	   {
+	    if(sum<3)
+		     return sum;
+		if(sum<5 && sum>=3)
+		   return number_of_coin(sum%3)+sum/3;
+	    if(sum>=5)
+		 return number_of_coin(sum%5)+sum/5;
+     }
+	int main(void)
+  {
+		long int sum;
+		read sum;
+		
+		out number_of_coin(sum);
+		
+		return 0;
+	}
+
