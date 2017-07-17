00eeff7185bf1bc5afc51a1254733f357c0db10c
jayalakshmiM <jayal3393@gmail.com>
2017-07-17 14:57:00 +0530

    Create seed.c

---
+++ seed.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ int main(void)
+ {
+	   int t,mul=1,x;
+	   scanf("%d",&t);
+	    int y=t;
+	    if(t!=0)
+	     {
+	        while(y!=0)
+	         {
+	             x=y%10;
+	              y=y/10;
+	               mul=mul*x;
+	         }
+	        printf("%d",mul*t);
+	        }
+	           else
+	            {
+	             printf("0");
+	               }     
+	
+	                return 0;
+	         }
+
