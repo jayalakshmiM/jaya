c57460c6be7612cc22c716c126ae9fd2c2c25f29
jayalakshmiM <jayal3393@gmail.com>
2017-07-17 09:55:11 +0530

    Create revwrd.java

---
+++ revwrd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class revwrd
+{
+	public static void main(String[] args)
+	{
+		String original,orev="";
+		   System.out.println("ENTER A STRING :");
+		   Scanner s=new Scanner(System.in);
+		   original=s.nextLine();
+		   String[] Arr=original.split(" ");
+		   for(int i=Arr.length-1;i>=0;i--)
+		     {
+		       orev=orev+" "+Arr[i];
+		     }
+		     System.out.println("REVERSED ORDER OF WORDS IN STRING IS:\n"+orev);
+	}
+
+}
